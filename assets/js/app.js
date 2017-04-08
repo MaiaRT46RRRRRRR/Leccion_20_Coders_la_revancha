9d9ca653508e2998d7605aeb00a1c2947a80943d
Maia <maia.rt.46@gmail.com>
2017-04-08 04:09:56 -0500

    sin sueño

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,25 @@
 window.onload=function(){
+	//Array de todas las estudiantes .
+	var coders=["Fiorella Quispe","Lourdes Vilchez","Rossmery Maldonado","Yessenia Burga",
+                  "Miriam Mendoza","Elizabeth Condori","Arantza Burga","Grecia Rayme",
+                  "Janine Vega","Rosario Felix","Daguiana Revoredo","Jenny Velasquez",
+                  "Nadia Cuadros","Michell More","Marilu Llamocca","Mariel Garcia",
+                  "Fiorella Cisneros","Geraldine Chauca","Yelitza Rivera","Stephanie Hiyagon",
+                  "Emma Tapia","Danna Franco","Flor Retamozo","Nathaly Pacheco",
+                  "Ericka Vidal","Katherine Ortega","Brilly Majuan","Flor Tello",
+                  "Leslie Avendaño", "Cindy Mendoza","Annia Flores","Betsi Loayza",
+                  "Aida Sulca","Milagros Gutierrez","Nakarid Jave","Angie Cóndor",
+                  "Maia Rojas","Ariana Cabana","Flor Condori","Mitch Rodríguez",
+                  "Naomi Villanueva","Mari Castillo","Miriam Peralta","Karin Alejo",
+                  "Liliana Peña","Ruth Salvador","Maribel Sevilla","Wendy Reyes",
+                  "Cinthia Stark","Maria Grecia Cutipa","Ana Durand","Glisse Jorge",
+                  "Neiza Nuñez","Sandra Solorzano"];
 
-	//var mostrar=document.
-	var coders=["Fiorella Quispe","Lourdes Vilchez"];
     var  bloq = document.getElementById("promocion");
 
   	bloq.innerHTML="";
 				
-		for(var i=1 ;i==2;i++){
+		for(var i=1 ;i<54;i++){
 
 			var newDiv =document.createElement('div');
 			newDiv.classList.add("grid");
@@ -15,7 +28,7 @@ window.onload=function(){
 			figure.classList.add("coder");
 
 			var newDiv2 =document.createElement("div");
-			newDiv.classList.add("overlay");
+			newDiv2.classList.add("overlay");
 
 			var imagen =document.createElement("img");
 			imagen.classList.add("img-box");
@@ -24,18 +37,18 @@ window.onload=function(){
 			newfig.classList.add("text");
 
 			var name = document.createTextNode(coders[i]); 
-			    imagen.setAttribute("src","assets/img/"+ i+".png");
+			    imagen.setAttribute("src","assets/img/"+ [i+1]+".png");
 
 						newfig.appendChild(name)
-						figure.appendChild(newfig);
+						figure.appendChild(newDiv2);
 						figure.appendChild(imagen);
-  						figure.appendChild(newDiv2);
+  						figure.appendChild(newfig);
   						newDiv.appendChild(figure);
 
   						bloq.appendChild(newDiv) ;
 		}
 		//document.getElementById("promocion").style.display="inline-block";
-}		
+}	;	
 
 
 
